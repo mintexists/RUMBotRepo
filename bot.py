a3b2297313471856abfe3672b78f5afa455eb886
CarlTheLoafLord <42393066+CarlTheLoafLord@users.noreply.github.com>
2020-08-19 21:57:04 -0400

    i made edits
    epic edits

--- bot.py
+++ bot.py
@@ -9,6 +9,41 @@ import math
 import traceback
 
 
+###---------------------------------------------------------------------------- GAME STUFF
+amazonlinks = ['https://www.amazon.com/Secret-Hitler/dp/B01JKD4HYC/ref=sr_1_4?dchild=1&keywords=weird+stuff&qid=1597776538&sr=8-4',
+               'https://www.amazon.com/CASOFU-Tortilla-Comfortable-Burrito-60inches/dp/B07QTHK8K9/ref=sxin_7_ac_d_rm?ac_md=0-0-d2VpcmQgc3R1ZmY%3D-ac_d_rm&cv_ct_cx=weird+stuff&dchild=1&keywords=weird+stuff&pd_rd_i=B07QTHK8K9&pd_rd_r=5c084d14-ef4f-477f-870b-b7015b607c97&pd_rd_w=7PiKe&pd_rd_wg=oKTaU&pf_rd_p=e3dc9e0c-9eab-4c3e-b43a-ba36f8522e14&pf_rd_r=4YWMA6JTVJ3X7P1B8C1W&psc=1&qid=1597776538&sr=1-1-12d4272d-8adb-4121-8624-135149aa9081',
+               'https://www.amazon.com/How-Talk-Your-About-Safety/dp/045149492X/ref=sr_1_10?dchild=1&keywords=weird+stuff&qid=1597776538&sr=8-10',
+               'https://www.amazon.com/Cooking-Semen-Delicious-Recipes-Inappropriate/dp/1091069387/ref=sr_1_13?dchild=1&keywords=weird+stuff&qid=1597776538&sr=8-13',
+               'https://www.amazon.com/Otamatone-Touch-Sensitive-Electronic-Musical-Instrument/dp/B00MRJ8GXK/ref=sr_1_14_sspa?dchild=1&keywords=weird+stuff&qid=1597776538&sr=8-14-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEzNTQzTjdRTjNQNlU5JmVuY3J5cHRlZElkPUEwMTExNTM2MTNERlNHM1hFTjE4NSZlbmNyeXB0ZWRBZElkPUEwNDU1NjI1MTNWMTlYMlAzTzdZOSZ3aWRnZXROYW1lPXNwX210ZiZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU=',
+               'https://www.amazon.com/Dodecagon-12-Side-Relieves-Depression-Children/dp/B071CFZ4BD/ref=sr_1_25?dchild=1&keywords=weird+stuff&qid=1597776538&sr=8-25',
+               'https://www.amazon.com/CelebriDucks-Harry-Ponder-Wizard-RUBBER/dp/B01GSOTFMA/ref=sr_1_30_sspa?dchild=1&keywords=weird+stuff&qid=1597776538&sr=8-30-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEzNTQzTjdRTjNQNlU5JmVuY3J5cHRlZElkPUEwMTExNTM2MTNERlNHM1hFTjE4NSZlbmNyeXB0ZWRBZElkPUEwMjE3OTcyVzEzNlFZVFZHUSZ3aWRnZXROYW1lPXNwX210ZiZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU=',
+               'https://www.amazon.com/PIZOFF-Pullover-Drawstring-Sweatshirts-AM006-01-L/dp/B07HGYVM55/ref=sr_1_35?dchild=1&keywords=weird+stuff&qid=1597776538&sr=8-35',
+               'https://www.amazon.com/Rubies-Conical-Alien-Skull-Flesh/dp/B001K3A45M/ref=sr_1_54?dchild=1&keywords=weird+stuff&qid=1597776538&sr=8-54',
+               'https://www.amazon.com/Catholic-Hipster-Handbook-audiobook/dp/B07CVKHCLR/ref=sr_1_55?dchild=1&keywords=weird+stuff&qid=1597776538&sr=8-55',
+               'https://www.amazon.com/Simulation-Pillow-Lumbar-Cushion-Stuffed/dp/B07SHP29DM/ref=sr_1_59_sspa?dchild=1&keywords=weird+stuff&qid=1597776538&sr=8-59-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEzNTQzTjdRTjNQNlU5JmVuY3J5cHRlZElkPUEwMTExNTM2MTNERlNHM1hFTjE4NSZlbmNyeXB0ZWRBZElkPUEwOTYwNjU5MjRUMFFUSzQwNEhERyZ3aWRnZXROYW1lPXNwX2J0ZiZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU=',
+               'https://www.amazon.com/Bluetooth-BCELIFE-Hands-Free-Rechargeable-Gifts%C2%A3%C2%A8Gray%C2%A3/dp/B075KTTKXS/ref=sr_1_58_sspa?dchild=1&keywords=weird+stuff&qid=1597776538&sr=8-58-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEzNTQzTjdRTjNQNlU5JmVuY3J5cHRlZElkPUEwMTExNTM2MTNERlNHM1hFTjE4NSZlbmNyeXB0ZWRBZElkPUEwODA4OTYxMU1DQVRHSTlDVTZJVCZ3aWRnZXROYW1lPXNwX2J0ZiZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU=',
+               'https://www.amazon.com/Villager-News/dp/B01I2NXNE6/ref=sr_1_1?dchild=1&keywords=minecraft&qid=1597776724&sr=8-1',
+               'https://www.amazon.com/Mattel-Games-FPD61-Minecraft-Card/dp/B074RCV4HQ/ref=sr_1_6?dchild=1&keywords=minecraft&qid=1597776733&sr=8-6',
+               'https://www.amazon.com/Jay-Franco-Minecraft-Quilt-Piece/dp/B072L3GMZV/ref=sr_1_13_sspa?dchild=1&keywords=minecraft&qid=1597776743&sr=8-13-spons&psc=1&smid=A3G62KWEAWSGV9&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUFLOTM3OVhFVEJEVk0mZW5jcnlwdGVkSWQ9QTAxMzkxMjUzR1VIMEpLTk81TEQyJmVuY3J5cHRlZEFkSWQ9QTA3MDAwOTM4MENMMkRPVTNVQ0gmd2lkZ2V0TmFtZT1zcF9tdGYmYWN0aW9uPWNsaWNrUmVkaXJlY3QmZG9Ob3RMb2dDbGljaz10cnVl',
+               'https://www.amazon.com/Basics-Of-Fly-Fishing/dp/B07HKSTWBX/ref=sr_1_23?dchild=1&keywords=fishing&qid=1597776755&sr=8-23',
+               'https://www.amazon.com/Clip-Roblox-Saving-Memes-Game/dp/B0837JN2FC/ref=sr_1_2?dchild=1&keywords=memes&qid=1597776791&sr=8-2']
+
+embarrass = ['I pissed the bed last night!',
+             'I listen to 100 gecs!',
+             'I have a iphone 5s!',
+             'I love Sword Art Online!',
+             'I play sexual vr games!',
+             'I Googled myself!',
+             'I said you too when a staff worker told me to enjoy the movie!',
+             'I take rewear filthy clothes from my hamper',
+             'I do not brush my teeth!',
+             'I think that big chungus is funny!',
+             'I like to pretend that I am apart of a different culture to make myself look cool!',
+             'I say pog irl unironically!',
+             'I still say cringy unironically!',
+             'I say and type XD unironically']
+###---------------------------------------------------------------------------- END OF GAME STUFF
+
 prefix = "r?"
 
 randNum = random.random()
@@ -28,7 +63,7 @@ async def checkSuggestions():
         for message in bot.suggestQueue:
             approvalsObject=get(message.reactions, emoji="✅")
             denialsObject=get(message.reactions, emoji="❌")
-            approvals=approvalsObject.count-(bot.user in set(await approvalsObject.users().flatten())) #gets # of yes reactions that isn't the bot
+            approvals=approvalsObject.count-(bot.user in set(await approvalsObjoect.users().flatten())) #gets # of yes reactions that isn't the bot
             denials=denialsObject.count-(bot.user in set(await denialsObject.users().flatten()))     #gets # of no reactions that isn't the bot
             timeLimit=datetime.timedelta(seconds=6*3600*(1-(approvals+denials)/bot.memberCount))            #math to figure out the time limit of the suggestion - 0 people reacted yet=6 hrs
             if (message.created_at.utcnow()-message.created_at)>timeLimit:
@@ -72,6 +107,32 @@ async def getLine(fileName,lineNum):
 @bot.event
 async def on_message(message):
     command = message.content.lower()
+    
+    ##GAMES
+    # guessing
+    if message.content.startswith("r?guess"):
+        channel = message.channel
+        await channel.send("Guess the number between 0-10 by typing it! (it will end once you guess correctly)")
+        number1 = random.randint(1, 10)
+        print(number1)
+
+        number2 = str(number1)
+
+        def check(m):
+            return m.content == number2 and m.channel == channel
+
+        msg = await bot.wait_for('message', check=check)
+        await channel.send("Correct answer {.author}!".format(msg))
+        
+        # amazon
+    if message.content.startswith("r?amazon"):
+        funkylinks = random.choice(amazonlinks)
+        await message.channel.send(funkylinks)
+        
+        # embarrass me
+    if message.content.startswith('r?embarrassme'):
+        embar = random.choice(embarrass)
+        await message.channel.send(embar + ' from {}!'.format(message.author.mention))
 
     # Add reaction to the suggestions
     if message.channel.id == 737807052625412208:
