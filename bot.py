fbf56a6203469e894304abb0125c367241f87b84
Evalyn <bates.nevin@doversherborn.org>
2020-08-25 14:20:40 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -150,7 +150,7 @@ async def on_message(message):
             files.append(await each.to_file())
         if len(files) > 0:
             fileMessage = await bot.get_guild(700359436203458581).get_channel(718277944153210961).send(files=files)
-        embedVar.set_image(url = fileMessage.attachments[0].url)
+            embedVar.set_image(url = fileMessage.attachments[0].url)        
         await message.channel.send(embed=embedVar)
     
     if command.startswith(prefix + "logbreak"):
